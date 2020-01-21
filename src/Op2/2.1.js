59cf419bbeb82141a3877a610b26f58cde533faf
jbreemes <54798893+jbreemes@users.noreply.github.com>
2020-01-21 11:27:57 +0100

    Opdracht 1 klaar

---
+++ src/Op2/2.1.js
@@ -0,0 +1,5 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("pepelaugh").src = "pic/pepelaugh.jpg";
+document.getElementById("graph").src = "pic/graph.jfif";
+
---
+++ src/Op2/2.2.js
@@ -0,0 +1,13 @@
+document.getElementById("bl1").style.backgroundColor = "#00ccff";
+document.getElementById("bl1").style.padding = "20px";
+
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.marginTop = "20px";
+document.getElementById("bl2").style.paddingTop = "1px";
+document.getElementById("bl2").style.paddingLeft = "20px";
+
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.marginTop = "50px";
+document.getElementById("bl3").style.paddingLeft = "45px";
+
+document.getElementById("bl4").style.backgroundColor = "red";
