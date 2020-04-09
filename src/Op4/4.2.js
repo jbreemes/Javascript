6a755fed8a141b3ee2a5f4777af0b0593f15c182
jozua <jbreemes@roc-dev.com>
2020-04-09 14:32:42 +0200

    Fixed

--- src/Op4/4.2.js
+++ src/Op4/4.2.js
@@ -1,17 +1,17 @@
-var plaatjes = [1,2,3,4,5,6,7,8,9,];
+var plaatjes = [1,2,3,4,5,6,7,8,9];
 var teller = 0;
-var sliderholder = document.getElementById("slideholder");
-sliderholder.style.background = "url(aap1.jpg)";
+var slideholder = document.getElementById("slideholder");
+slideholder.style.backgroundImage =  "url(img/dier9.jpg";
 
-sliderholder.addEventListener("click", function(){
-    sliderholder.style.background = "url(aap" + getAap() + ".jpg)";
+slideholder.addEventListener("click", function () {
+    slideholder.style.backgroundImage = "url(img/dier" + getDier() + ".jpg)";
 });
 
-function getAap() {
+function getDier() {
     if(teller >= plaatjes.length){
         teller = 1;
     } else {
-        teller ++;
+        teller++;
     }
     console.log(teller);
     return teller;
